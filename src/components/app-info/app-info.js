860cb31b4ec26b4cb72387d98813ddeeb2542155
bfmvenom45 <bfmvenom45@gmail.com>
2025-07-09 13:52:45 +0300

    Initial commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Кількість праціників в компанії N</h1>
+            <h2>Сумарна к-сть працівників:</h2>
+            <h2>Пакет допомоги:</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
